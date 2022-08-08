b0dd02ada8d8e99d122503dea277a2ad5382e27f
Gabriel Steven Machicao Quispe <gmachicaoq@unsa.edu.pe>
2022-08-08 15:28:05 -0500

    Se modifica Aplicacion1, agregando un modelo de datos Productos.

--- src/Proyecto/Apps/Aplicacion1/apps.py
+++ src/Proyecto/Apps/Aplicacion1/apps.py
@@ -3,4 +3,4 @@ from django.apps import AppConfig
 
 class Aplicacion1Config(AppConfig):
     default_auto_field = 'django.db.models.BigAutoField'
-    name = 'Aplicacion1'
+    name = 'Apps.Aplicacion1'
--- src/Proyecto/Proyecto/settings.py
+++ src/Proyecto/Proyecto/settings.py
@@ -38,6 +38,7 @@ INSTALLED_APPS = [
     'django.contrib.messages',
     'django.contrib.staticfiles',
     'rest_framework',
+    'Apps.Aplicacion1'
 ]
 
 MIDDLEWARE = [
